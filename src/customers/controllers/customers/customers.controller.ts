ebd0e0bbab075b233e0baa530ae2567694ac87b7
choudel <chouaibh@gmail.com>
2022-07-12 09:45:42 +0100

    created post method

--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { CreateCustomerDto } from 'src/customers/dtos/CreateCustomer.dto';
 import { CustomersService } from 'src/customers/services/customers/customers.service';
 
 @Controller('customers')
 export class CustomersController {
     constructor(private customerService: CustomersService){}
+
     @Get('/search/:id')
     getCustomer(
         @Param('id',ParseIntPipe) id: number
@@ -12,5 +14,10 @@ export class CustomersController {
       if(customer){
         return customer
       }else throw new HttpException('Customer Not Found!', HttpStatus.BAD_REQUEST)  
+    }
+    @Post('create')
+    createCustomer(@Body() createCustomerDto: CreateCustomerDto){
+        console.log(createCustomerDto);
+        this.customerService.createCustomer(createCustomerDto);
     }
 }
--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { CreateCustomerDto } from 'src/customers/dtos/CreateCustomer.dto';
+import { Customer } from 'src/customers/types/Customer';
 
 @Injectable()
 export class CustomersService {
-    users = [
+    private customers:Customer[] = [
         {
             id: 1,
             email: 'danny@gmail.com',
-            createdAt: new Date()
+            name: 'shlomo'
         },
         {
             id: 2,
             email: 'johny@gmail.com',
-            createdAt: new Date()
+            name: 'doe johnny'
         },
         {
             id: 3,
             email: 'dom@gmail.com',
-            createdAt: new Date()
+            name: 'Rojo Dom'
         },
 
     ]
     findCustomerById(id:number) {
-        return this.users.find((user)=> user.id===id);
+        return this.customers.find((user)=> user.id===id);
+    }
+    createCustomer(customerDto:CreateCustomerDto){
+        this.customers.push(customerDto)
     }
 }
