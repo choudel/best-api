837867651855e5be04438305b4cece49d0ce5a70
choudel <chouaibh@gmail.com>
2022-08-04 20:57:37 +0200

    connect with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomersModule } from './customers/customers.module';
+import entities from './typeorm';
 import { UsersModule } from './users/users.module';
 
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [CustomersModule, UsersModule, TypeOrmModule.forRoot(
+    {
+      type:'mysql',
+      host:'localhost',
+      port:3306,
+      username:'root',
+      password:'',
+      database:'tuto_db',
+      entities:entities,
+      synchronize:true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
